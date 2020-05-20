04549f2528572958c9634f6d32828c923b3f7ce5
unknown <gerson.villugas.a@uni.pe>
2020-05-20 18:46:15 -0500

    update

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,11 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV=process.env.NODE_ENV || 'dev'
 //base de datos
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDB = 'mongodb://localhost:27017/cafe'
-//} else {
+if (process.env.NODE_ENV === 'dev') {
+   urlDB = 'mongodb://localhost:27017/cafe'
+} else {
     //admin_cafe
     //XIYRHH6YO2tB79rJ
     urlDB='mongodb+srv://admin_cafe:XIYRHH6YO2tB79rJ@cluster0-mmg6n.mongodb.net/cafe'
-//}
+}
 process.env.urlDB=urlDB;
